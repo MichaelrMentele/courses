c9eddb866f2161aedfb2340492111b5bc9026318
michaelrmentele <michaelrmentele@gmail.com>
2017-09-26 19:16:43 -0700

    Front page HTML now generated from a template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -35,7 +35,7 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do did not appear in table"
         )
 
         # There is stall a text box inviting her to add more todos
